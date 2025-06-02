91dbd36d6b89d9e6ee8a7f625e518d1cc94a4878
OSTYADO <clamourostyado@gmail.com>
2025-06-03 02:05:18 +0300

    Update index.js

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.listen(PORT, () => {
     console.log(`
-Don't Forget To Give Star TONIC-MD
+LEAVE A STAR FOR FLUXION-II MD
 
  Server running on http://localhost:` + PORT)
 })
